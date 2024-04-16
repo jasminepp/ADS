fb718124b0681240640f16bf727ced3fdfd1b72e
chen <nz20262@bristol.ac.uk>
2024-04-16 19:54:32 +0100

    stage 0

--- data_processing/stage0.py
+++ data_processing/stage0.py
@@ -1,30 +1,31 @@
 import pandas as pd
-from pathlib import Path
 
-folder_list = []
-train_dir = Path('train')
+for i in range(10, 11):  
+    directory = f'{i:05}' 
 
-for folder in train_dir.iterdir():
-    folder_list.append(folder)
+    df_xyz = pd.read_csv(f'train/{directory}/acceleration_clean.csv')
+    df_pir = pd.read_csv(f'train/{directory}/pir_clean.csv')
+    df_targets = pd.read_csv(f'train/{directory}/targets.csv')
+    output_file = f'train/{directory}/train_stage0.csv'
 
+    def row_to_vector(row):
+      index_of_one = row.idxmax()
+      return row.index.get_loc(index_of_one)
 
-for folder in folder_list:
-    df_xyz = pd.read_csv(folder/'acceleration_clean.csv')
-    df_pir = pd.read_csv(folder/'pir_clean.csv')
-    df_targets = pd.read_csv(folder/'targets.csv')
-    output_file = folder/'train_stage0.csv'
+    # process pir
+    pir_columns = df_pir.columns.difference(['start', 'end'])
+    df_pir['location_idx'] = df_pir[pir_columns].apply(lambda x: x.tolist(), axis=1)
 
-    df_pir['location_vector'] = df_pir.drop(['start', 'end'], axis=1).apply(lambda x: x.tolist(), axis=1)
-    df_pir = df_pir[['start', 'end', 'location_vector']]
+    df_pir = df_pir[['start', 'end', 'location_idx']]
 
-    
-    df_targets['target_vector'] = df_targets.drop(['start', 'end'], axis=1).apply(lambda x: x.tolist(), axis=1)
+    # transfer target to vector
+    target_columns = df_targets.columns.difference(['start', 'end'])
+    df_targets['target_vector'] = df_targets[target_columns].apply(lambda x: x.tolist(), axis=1)
+
     df_targets = df_targets[['start', 'end', 'target_vector']]
-
+ 
     df_merged = df_pir.merge(df_xyz, on=['start', 'end'], how='outer') \
                       .merge(df_targets, on=['start', 'end'], how='outer')
-    
 
     df_merged.to_csv(output_file, index=False)
     print(f'Merged file saved as {output_file}')
-    
